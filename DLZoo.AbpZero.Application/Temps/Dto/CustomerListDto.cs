64d8ca7ad83211fd6314957bf58ac76cb9c1d63e
Eric <yjp1314@126.com>
2019-04-30 16:19:59 +0800

    add new dto

---
+++ DLZoo.AbpZero.Application/Temps/Dto/CustomerListDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.AutoMapper;
+using Abp.Domain.Entities;
+using MyTempProject.
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTempProject.Temps.Dto
+{
+    [AutoMapFrom(typeof(Entities.CCustomer)]
+    public class CustomerListDto : Entity<int>
+    {
+        public virtual string customerName { get; set; }
+        public virtual string address { get; set; }
+        public virtual string tel { get; set; }
+    }
+}
