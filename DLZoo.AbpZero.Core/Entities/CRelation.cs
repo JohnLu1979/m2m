3726f6efa164f2840808f2e95c3cb7d86b1fe187
John.lu <John.lu.1979@outlook.com>
2019-07-24 02:11:07 +0800

    add

---
+++ DLZoo.AbpZero.Core/Entities/CRelation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abp.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyTempProject.Entities
+{
+    [Table("relation")]
+   public class CRelation:Entity<int>
+    {
+        public virtual int? customer_id { get; set; }
+        public virtual int? site_id { get; set; }
+    }
+}
---
+++ DLZoo.AbpZero.Core/Entities/CUser.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTempProject.Entities
+{
+    [Table("user")]
+    public class CUser : Entity<int>
+    {
+        [MaxLength(50)]
+        public virtual string username { get; set; }
+        [MaxLength(50)]
+        public virtual string password { get; set; }
+        [MaxLength(50)]
+        public virtual string address { get; set; }
+        [MaxLength(50)]
+        public virtual string tel { get; set; }
+    }
+}
