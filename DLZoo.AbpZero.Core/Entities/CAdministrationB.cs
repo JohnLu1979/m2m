9b0bc949f1e29fc59670c29830b51b31a0ee1d81
eric <yjp1314@126.com>
2019-07-24 21:07:54 +0800

    add new entity

---
+++ DLZoo.AbpZero.Core/Entities/CAdministrationB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTempProject.Entities
+{
+    [Table("adm_administration_b")]
+    public class CAdministrationB
+    {
+        [Required]
+        [MaxLength(50)]
+        public virtual string addvcd { get; set; }
+        
+        public virtual string addvname { get; set; }
+
+        public virtual string parentcd { get; set; }
+
+        public virtual string leafflag { get; set; }
+
+        public virtual string levels { get; set; }
+
+        public virtual string areaid { get; set; }
+    }
+}
