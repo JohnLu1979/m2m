659860e8de6ae192ae9480b892f35ecf575bc59b
eric <yjp1314@126.com>
2019-07-24 22:51:50 +0800

    commit

--- DLZoo.AbpZero.Core/Entities/CAdministrationB.cs
+++ DLZoo.AbpZero.Core/Entities/CAdministrationB.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Abp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 namespace MyTempProject.Entities
 {
     [Table("adm_administration_b")]
-    public class CAdministrationB
+    public class CAdministrationB : Entity<string>
     {
         [Required]
         [MaxLength(50)]
